30f9fc087a6a159a6e40dd635bee1ed0d9c47eb9
Julian Baisch <julian-baisch@web.de>
2023-12-08 13:36:32 +0100

    wip data from flask into line chart

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
+  <h1>Business Intelligence Vue Project</h1>
   <div>
-    <Line :data="lineChartData" :options="options" />
-  </div>
-  <div>
-    <Bar :data="barChartData" :options="options" />
-  </div>
-  <div>
-    <Doughnut :data="doughnutChartData" :options="options" />
+    <Line :data="chartData" :options="chartOptions" />
   </div>
 </template>
 
 <script lang="ts">
+import axios from 'axios';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -24,8 +20,6 @@ import {
   ArcElement
 } from 'chart.js'
 import { Line, Bar, Doughnut } from 'vue-chartjs'
-import * as chartConfig from './config/chartConfig.js'
-import * as doughnutchartConfig from './config/doughnutchartConfig.js'
 
 ChartJS.register(
   CategoryScale,
@@ -46,14 +40,55 @@ export default {
   },
   data() {
     return {
-      lineChartData: chartConfig.data,
-      barChartData: chartConfig.data,
-      doughnutChartData: doughnutchartConfig.data,
-      options: {
+      chartData: {
+        labels: [],
+        datasets: [
+          {
+            label: 'Market Price',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            data: [],
+          },
+        ],
+      },
+      chartOptions: {
         responsive: true,
         maintainAspectRatio: false,
-      }
+      },
     };
+  },
+  mounted() {
+    this.fetchData();
+  },
+  methods: {
+    async fetchData() {
+      try {
+        const response = await axios.get('http://localhost:5000/api/get_data');
+        console.log(response);
+        this.processData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    },
+    processData(data) {
+      // Process the data if needed
+      // You might want to format timestamps, convert types, etc.
+      this.chartData = {
+        labels: data.map(entry => entry.start_timestamp),
+        datasets: [
+          {
+            label: 'Market Price',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            data: data.map(entry => entry.marketprice),
+          },
+        ],
+      };
+      this.chartOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+      };
+    }
   }
 }
 </script>
--- src/backend.py
+++ src/backend.py
@@ -1,17 +1,40 @@
+from datetime import datetime
 from flask import Flask, jsonify
 from flask_cors import CORS
+import json
 import pandas as pd
+import requests
 
 app = Flask(__name__)
 CORS(app)
 
-# Create a simple Pandas DataFrame for demonstration
-data = {"Name": ["John", "Jane", "Bob"], "Age": [25, 30, 22]}
-df = pd.DataFrame(data)
+api_url = "https://api.awattar.de/v1/marketdata"
+df = None
+
+# Erstelle die Parameter für "start" und "end" in Millisekunden
+start_timestamp = int(datetime(2023, 1, 1).timestamp() * 1000)
+end_timestamp = int(datetime(2023, 1, 31).timestamp() * 1000)
 
 
 @app.route("/api/get_data", methods=["GET"])
 def get_data():
+    params = {"start": start_timestamp, "end": end_timestamp}
+
+    response = requests.get(api_url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        df = pd.DataFrame(data["data"])
+        df["marketprice"] = (df["marketprice"] * 0.1).round(2)
+        df["unit"] = "Cent/kWh"
+
+        # Speichere die JSON-Daten in einer Datei
+        # with open('api_response.json', 'w') as json_file:
+        #     json.dump(data, json_file, indent=4)
+    else:
+        print(f"Fehler bei der Anfrage. Statuscode: {response.status_code}")
+        return "", 500
     return jsonify(df.to_dict(orient="records"))
 
 
