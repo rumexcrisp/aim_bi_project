899e3396d2218a325f8521d61b612a02ef1f0749
Julian Baisch <julian-baisch@web.de>
2024-01-04 17:56:59 +0100

    wip: refactor backend

--- src/backend.py
+++ src/backend.py
@@ -1,41 +1,50 @@
 from datetime import datetime
 from flask import Flask, jsonify
 from flask_cors import CORS
-import json
-import pandas as pd
-import requests
+
+from utils import read_db, get_data, convert_to_germany_time, connect_db
 
 app = Flask(__name__)
 CORS(app)
 
-api_url = "https://api.awattar.de/v1/marketdata"
-df = None
+sqlite_db = "'../data/db.sqlite'"
 
 # Erstelle die Parameter für "start" und "end" in Millisekunden
-start_timestamp = int(datetime(2023, 1, 1).timestamp() * 1000)
-end_timestamp = int(datetime(2023, 1, 31).timestamp() * 1000)
+# start_timestamp = int(datetime(2023, 1, 1).timestamp() * 1000)
+# end_timestamp = int(datetime(2023, 1, 31).timestamp() * 1000)
 
 
 @app.route("/api/get_data", methods=["GET"])
-def get_data():
-    params = {"start": start_timestamp, "end": end_timestamp}
+def provide_data(start=datetime(2019, 1, 1), end=datetime(2020, 1, 1)):
+    conn = connect_db(sqlite_db)
+    df = read_db(conn, start, end)
 
-    response = requests.get(api_url, params=params)
-
-    if response.status_code == 200:
-        data = response.json()
-
-        df = pd.DataFrame(data["data"])
-        df["marketprice"] = (df["marketprice"] * 0.1).round(2)
-        df["unit"] = "Cent/kWh"
-
-        # Speichere die JSON-Daten in einer Datei
-        # with open('api_response.json', 'w') as json_file:
-        #     json.dump(data, json_file, indent=4)
-    else:
-        print(f"Fehler bei der Anfrage. Statuscode: {response.status_code}")
+    if df.empty:
+        print("Fehler, dataframe leer...")
         return "", 500
     return jsonify(df.to_dict(orient="records"))
+
+
+def setup() -> None:
+    conn = connect_db(sqlite_db)
+    start = int(datetime(2010, 1, 1).timestamp() * 1000)
+    end = int(datetime(2023, 1, 1).timestamp() * 1000)
+    df = get_data(start, end)
+    assert not df.empty
+
+    df["marketprice"] = (df["marketprice"] * 0.1).round(2)
+    df["unit"] = "Cent/kWh"
+    df["date"] = df["start_timestamp"].apply(lambda epoch: convert_to_germany_time(epoch))
+
+    df_db = read_db(conn)
+    if df.equals(df_db):
+        print("DB is already available and populated, nothing to save...")
+        return
+
+    try:
+        df.to_sql('awattar', conn, if_exists='replace', index=False)
+    except Exception as e:
+        print(f"Error writing to sqlite DB during setup: {e}")
 
 
 if __name__ == "__main__":
---
+++ src/utils.py
@@ -0,0 +1,108 @@
+from datetime import datetime, timezone
+import json
+import pandas as pd
+import requests
+import time
+import pytz
+import sqlite3
+import os
+
+
+def connect_db(db) -> sqlite3.Connection | None:
+    """return db handle
+
+    Args:
+        db (string): path to sql file
+
+    Returns:
+        sqlite3.Connection | None: db handle
+    """
+    try:
+        if os.path.exists(db) and os.path.isfile(db):
+            return sqlite3.connect(db)
+        else:
+            raise FileNotFoundError
+    except Exception as e:
+        print(f"Error connection to db: {db}: {e}")
+        return 1
+
+
+def get_data(start, end) -> pd.DataFrame:
+    """get data from awattar
+
+    Args:
+        start (int): epoch in ms
+        end (int): epoch in ms
+
+    Returns:
+        pd.DataFrame: dataframe containing data
+    """
+    api_url = "https://api.awattar.de/v1/marketdata"
+    df = pd.DataFrame()
+
+    start_timestamp = start
+    end_timestamp = end
+
+    params = {
+        'start': start_timestamp,
+        'end': end_timestamp
+    }
+
+    print("Trying to get data from awattar api")
+
+    start_time = time.time()
+    response = requests.get(api_url, params=params)
+    end_time = time.time()
+
+    print(f"took {end_time - start_time:.6f} seconds to execute.")
+
+    if response.status_code == 200:
+        data = response.json()
+
+        with open(f'../data/api_response_{start}_{end}.json', 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+
+        return pd.DataFrame(data["data"])
+    else:
+        print(f"Fehler bei der Anfrage. Statuscode: {response.status_code}")
+        return df
+
+
+def convert_to_germany_time(epoch) -> str:
+    """converts input epoch to german time string
+
+    Args:
+        epoch (int): epoch in ms
+
+    Returns:
+        str: german time string
+    """
+    germany_timezone = pytz.timezone("Europe/Berlin")
+    dt_utc = datetime.fromtimestamp(epoch / 1000, tz=timezone.utc)
+    dt_germany = dt_utc.replace(tzinfo=pytz.utc).astimezone(germany_timezone)
+    return dt_germany.strftime('%d-%m-%Y %H:%M:%S %Z')
+
+
+def read_db(conn, start=datetime(1970, 1, 1, 1), end=datetime(1970, 1, 1, 1)) -> pd.DataFrame | None:
+    """read data from sqlite db, returns whole db if start and end are default
+
+    Args:
+        conn (sqlite3.connect): sqlite3 db
+        start (time.datetime, optional): start time. Defaults to datetime(1970, 1, 1, 1).
+        end (time.datetime, optional): end time. Defaults to datetime(1970, 1, 1, 1).
+
+    Returns:
+        pd.DataFrame | None: _description_
+    """
+    start = int(start.timestamp() * 1000)
+    end = int(end.timestamp() * 1000)
+    print(f"start: {start}, end: {end}")
+    df_db = pd.DataFrame()
+    start_time = time.time()
+    df_db = pd.read_sql('select * from awattar', conn)
+    print(f"db read took {(time.time()) - start_time:.6f} seconds to execute.")
+    if start == 0 and end == 0:
+        return df_db
+    elif start <= end:
+        df_db = df_db[(df_db['start_timestamp'] >= start) & (df_db['start_timestamp'] < end)]
+        return (df_db)
