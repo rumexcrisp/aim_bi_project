4c16c7665a2176ed04f83fb55c802db2e316a164
Julian Baisch <julian-baisch@web.de>
2024-01-05 14:38:58 +0100

    show chart only when data ready

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,39 @@
 <template>
-  <h1>Business Intelligence Vue Project</h1>
-  <div>
-    <Line :data="chartData" :options="chartOptions" />
+  <h1>Business Intelligence Project</h1>
+  <div class="container">
+    <BarChart v-if="loaded" id="line" :options="chartOptions" :data="chartData">Chart couldn't be loaded.</BarChart>
   </div>
 </template>
 
 <script lang="ts">
 import axios from 'axios';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  ArcElement
-} from 'chart.js'
-import { Line, Bar, Doughnut } from 'vue-chartjs'
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  BarElement,
-  ArcElement,
-  Title,
-  Tooltip,
-  Legend
-)
+import BarChart from './components/BarChart.vue';
+import LineChart from './components/LineChart.vue';
 
 export default {
   name: 'App',
   components: {
-    Line, Bar, Doughnut
+    BarChart, LineChart
   },
-  data() {
-    return {
-      chartData: {
-        labels: [],
-        datasets: [
-          {
-            label: 'Market Price',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            data: [],
-          },
-        ],
-      },
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-      },
-    };
-  },
+
+  data: () => ({
+    loaded: false,
+    chartData: null,
+    chartOptions: null
+  }),
+
   mounted() {
+    this.loaded = false;
     this.fetchData();
   },
+
   methods: {
     async fetchData() {
       try {
         const response = await axios.get('http://127.0.0.1:5000/api/get_data');
         console.log(response);
         this.processData(response.data);
+        this.loaded = true;
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -74,7 +42,7 @@ export default {
       // Process the data if needed
       // You might want to format timestamps, convert types, etc.
       this.chartData = {
-        labels: data.map(entry => entry.start_timestamp),
+        labels: data.map(entry => entry.date),
         datasets: [
           {
             label: 'Market Price',
---
+++ src/components/BarChart.vue
@@ -0,0 +1,25 @@
+<template>
+    <Bar id="my-chart-id" :data="chartData" :options="chartOptions" />
+</template>
+
+<script lang="ts">
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+    name: 'BarChart',
+    components: { Bar },
+    props: {
+        chartData: {
+            type: Object,
+            required: true
+        },
+        chartOptions: {
+            type: Object,
+            default: () => {}
+        }
+    }
+}
+</script>
